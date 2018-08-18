a17cf2110c4ebc986108b7c05cb9d9175a9e6a11
ljfirst <ljfirst@mail.ustc.edu.cn>
2018-08-19 02:28:18 +0800

    SinglyLinkedList new modify

--- data_structure/link_list/SinglyLinkedList.java
+++ data_structure/link_list/SinglyLinkedList.java
@@ -1,4 +1,8 @@
 package link_list;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /** 
  *@author liujun
  *@date： 2018-8-18 上午01:08:11
@@ -57,15 +61,33 @@ public class SinglyLinkedList {
 	}
 	 //2. 单链表逆置
 	 //  2.1 数组存放，倒序输出
-	public void arrayReverse(SinglyLinkedList first) {
+	/*public void arrayReverse(SinglyLinkedList first) {
 		// TODO Auto-generated method stub
 		System.out.println("数组存放，倒序输出");
-	}
+		int[] array = new int[16];
+		int count = 0;
+		int max = 16;
+		while (first != null) {
+			array[count] = first.value;
+			count++;
+			//处理数组越界问题
+			if(count == max){
+				int[] arraycopy = new int[max * 2];
+				for (int i : array) {
+					
+				}
+			}
+		}
+	}*/
 	
 	 //  2.2 利用栈倒叙输出(递归)
 	public void stackReverse(SinglyLinkedList first) {
 		// TODO Auto-generated method stub
-		System.out.println("利用栈倒叙输出(递归)");
+		//System.out.println("利用栈倒叙输出(递归)");
+		if(first.next != null){
+			stackReverse(first.next);
+		}
+		System.out.print(first.value);
 	}
 	
 	 //  2.3 反置链表，然后输出(非递归)
@@ -85,8 +107,15 @@ public class SinglyLinkedList {
 	
 	
 	 //  2.4 反置链表，然后输出(递归)
-	public void listReverseRecursion(SinglyLinkedList first) {
+	public SinglyLinkedList listReverseRecursion(SinglyLinkedList first) {
 		// TODO Auto-generated method stub
+		if(first == null || first.next == null){
+			return first;
+		}
+		SinglyLinkedList newhead = listReverseRecursion(first.next);
+		first.next.next = first;
+		first.next = null;
+		return newhead;
 	}
 	
 	//输出链表
@@ -100,11 +129,13 @@ public class SinglyLinkedList {
 	
 	//主方法测试
 	public static void main(String[] args) {
+//		Map  s = new HashMap<Integer, Integer>(12);
 		SinglyLinkedList first = new SinglyLinkedList(0);
 		int[] array = {1,2,3,4,5,6,7,8,9,10};
 		first = first.insertTail(array);
 		first.printLink(first);
-		first = first.listReverse(first);
+		System.out.println();
+		first = first.listReverseRecursion(first);
 		first.printLink(first);
 	}
 }
