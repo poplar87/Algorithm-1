7659ca97befd72d8990d65ac70e763057b13a988
ljfirst <ljfirst@mail.ustc.edu.cn>
2019-01-08 12:26:56 +0800

    dvalue

--- alg.other/other/D_Value.java
+++ alg.other/other/D_Value.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
  * 例如b2+b3+b4 = (a2-a3) + (a3-a4) + (a4-a5) = a2 - a5
  * O(n)构造出B序列后，用类似“最大子段和”算法求“最小绝对值子段和”（不是最简洁）。
  * 
- * 方法二：遍历一遍数据，找出最大值Max和最小值Min，然后把整个数据进行划分，step=(Max - Min)/n.然后遍历这n个桶，
+ * 方法二(废)：遍历一遍数据，找出最大值Max和最小值Min，然后把整个数据进行划分，step=(Max - Min)/n.然后遍历这n个桶，
  * 相邻元素的最大值一定是某个桶i中的最大值和桶(i+1)中的最小值的差值。满足Min + n * step = Max。
  *      反证法：假如这个相邻元素的最大间距不是某个桶 i 中的最大值和桶 (i+1) 中的最小值的差值，
  *      即最大间距的两个元素位于同一个桶中，即最大间距小于step，所以Min + n * step < Maxd的。
@@ -24,8 +24,23 @@ import java.util.Arrays;
  */
 public class D_Value {
 
-	//最小绝对值子段和
 	public void method1(int[] array) {
+		// TODO Auto-generated method stub
+		//排序
+		Arrays.sort(array);
+		int min = Integer.MAX_VALUE;
+		int temp = 0;
+		for (int i = 1; i < array.length; i++) {
+			temp = array[i] - array[i-1];
+			if(min > temp){
+				min = temp;
+			}
+		}
+		System.out.println(min);
+	}
+	
+	//最小绝对值子段和
+	public void method2(int[] array) {
 
 		//构造数组B=(b1,b2,...,bn-1)
 		int[] array_b = new int[array.length - 1];
@@ -37,7 +52,18 @@ public class D_Value {
 			}
 		}
 		//数组B 最小绝对值子段和
-		int start = 0;
+		int[] array_c = new int[array_b.length];
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < array_c.length; i++) {
+			for (int j = 0; j <= i; j++) {
+				array_c[j] += array_b[i];
+				if(Math.abs(min) > Math.abs(array_c[j])){
+					min = Math.abs(array_c[j]);
+				}
+			}
+		}
+		//数组B 最小绝对值子段和(有问题)
+		/*int start = 0;
 		int end = 0;
 		int start_temp = 0;
 		int temp_abs = 0;
@@ -49,6 +75,7 @@ public class D_Value {
 			sum_temp += array_b[i];
 			sum_temp_abs = sum_temp > 0 ? sum_temp : 0 - sum_temp;
 			temp_abs = array_b[i] > 0 ? array_b[i] : 0 - array_b[i]; 
+			
 			if (temp_abs < best_min) {
 				best_min = temp_abs;
 				start = i;
@@ -64,13 +91,13 @@ public class D_Value {
 					sum_temp = best_min;
 				}
 			}
-		}
+		}*/
 		//输出差值
-		System.out.println(best_min);
+		System.out.println(min);
 	}
 	
 	//桶方法:这个方法有极大的漏洞
-	public void method2(int[] array) {
+	/*public void method3(int[] array) {
 		// TODO Auto-generated method stub
 		//找出最大值Max和最小值Min
 		Arrays.sort(array);
@@ -90,7 +117,6 @@ public class D_Value {
 				while (array[start] < count) {
 					start++;
 				}
-				
 				if(start <= n - 1 && best_min > (array[start] - array[start - 1])){
 					best_min = array[start] - array[start - 1];
 				}
@@ -98,16 +124,19 @@ public class D_Value {
 			count += step;
 		}
 		System.out.println(best_min);
-	}
+	}*/
 	
 	//主方法测试
 	public static void main(String[] args) {
-		int[] array = {15,7,9,-640,80,-2,33,25,41,-5,84,6};
-		//期望输出9 - 8 = 1
+		int[] array = {15,7,-640,10,80,-2,33,25,41,-5,84,22};
+		int[] array1 = {15,7,-640,10,80,-2,33,25,41,-5,84,22};
+		int[] array2 = {15,7,-640,10,80,-2,33,25,41,-5,84,22};
+		
+		//期望输出10 - 7 = 3
 		D_Value dv = new D_Value();
-		System.out.println("方法一：最小绝对值子段和");
+		System.out.println("方法一：排序暴力差值");
 		dv.method1(array);
-		System.out.println("方法二：桶方法");
-		dv.method2(array);
+		System.out.println("方法二：最小绝对值子段和");
+		dv.method2(array1);
 	}
 }
